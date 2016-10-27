439b4eddb71b4dc2ec77752cd7c50300e41bf24c
Watanabe Keiichi <nabeon+github@gmail.com>
2016-10-27 18:07:13 +0900

    Fix get value from options.

--- ios/RNImageToBase64.m
+++ ios/RNImageToBase64.m
@@ -8,18 +8,21 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(getBase64String:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
 {
-  NSURL *url = [[NSURL alloc] initWithString:input];
+  NSString *url = [options valueForKey:@"url"];
+
   ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-  [library assetForURL:[[options valueForKey:@"url"] stringValue] resultBlock:^(ALAsset *asset) {
+  [library assetForURL:[[NSURL alloc] initWithString:url] resultBlock:^(ALAsset *asset) {
     ALAssetRepresentation *rep = [asset defaultRepresentation];
     CGImageRef imageRef = [rep fullScreenImage];
-    UIImage *img = [UIImage imageWithCGImage:imageRef]
+    UIImage *img = [UIImage imageWithCGImage:imageRef];
     NSData *imageData = UIImageJPEGRepresentation(img, [[options valueForKey:@"quality"] floatValue]);
     NSString *base64Encoded = [imageData base64EncodedStringWithOptions:0];
     callback(@[[NSNull null], base64Encoded]);
+
   } failureBlock:^(NSError *error) {
     NSLog(@"that didn't work %@", error);
     callback(@[error]);
+
   }];
 }
 
